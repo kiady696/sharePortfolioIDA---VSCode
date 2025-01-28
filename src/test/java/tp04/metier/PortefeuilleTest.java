bbe20b6d288941c64154e5b78da105d30b0536e9
ucantosabaga <127980287+ucantosabaga@users.noreply.github.com>
2025-01-28 11:31:03 +0100

    Creation du test ajout/supprime des actions

--- src/test/java/tp04/metier/PortefeuilleTest.java
+++ src/test/java/tp04/metier/PortefeuilleTest.java
@@ -1,5 +1,7 @@
 package tp04.metier;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +18,58 @@ class PortefeuilleTest {
         //Assert
         Assertions.assertEquals(expectedToString, currentToString, "Basic construction");
         
+    }
+
+    @Test
+    void testAddAndRemoveActions() {
+        // Arrange
+        Portefeuille portefeuille = new Portefeuille();
+
+        ActionSimple action1 = new ActionSimple("Action1");
+        ActionSimple action2 = new ActionSimple("Action2");
+
+        //Our test will include the insertion and removal of two actions in one Portefeuille
+
+
+        //Action
+        Jour jour1 = new Jour(2025, 1);
+        //We define the values of our two actions on a single day
+        action1.enrgCours(jour1, 10); 
+        action2.enrgCours(jour1, 20); 
+
+        // Assert
+
+        //We add the first action, then check if the action is correctly added by comparing it to our predefined String structure
+        portefeuille.acheter(action1, 10);
+        assertEquals("{Action1=10}", portefeuille.toString());
+
+        
+        //We add the second action, then check if both actions are represented in the correct way in order to verify the addition
+        portefeuille.acheter(action2, 20);
+        assertEquals("{Action1=10, Action2=20}", portefeuille.toString());
+
+        
+        //We add more to our first action and expect to see the addition on our Portefeuille.
+        portefeuille.acheter(action1, 5);
+        assertEquals("{Action1=15, Action2=20}", portefeuille.toString());
+
+        
+        //We remove from the first action, we expect to see the decrease on our action quantity
+        portefeuille.vendre(action1, 10);
+        assertEquals("{Action1=5, Action2=20}", portefeuille.toString());
+
+        
+        //Here we sell all the quantity of our first action, we expect to see only our second action and its value
+        portefeuille.vendre(action1, 5);
+        assertEquals("{Action2=20}", portefeuille.toString());
+
+        
+        //Here we sell again from the first action, we expect to have no effects on our Portefeuille since all of the action1 is sold
+        portefeuille.vendre(action1, 5);
+        assertEquals("{Action2=20}", portefeuille.toString());
+
+        //Here we sell the rest of the second action to verify the Portefeuille will be empty
+        portefeuille.vendre(action2, 20);
+        assertEquals("{}", portefeuille.toString());
     }
 }
